fe8b317efa9f97cd70211c1023871d983926b860
KyubinHwang <choco1drink@gmail.com>
2022-07-28 20:00:19 +0900

    주제 : 가족 오디오 업로드 3개

--- src/audio2/audio2-5.js
+++ src/audio2/audio2-5.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import styled from './Audio.module.css'
 import ReactAudioPlayer from 'react-audio-player';
-import sound from './audio/채수원3.m4a';
+import sound from './audio/정다운(어머니).m4a';
 import logo from './img/DAALL_logo.jpeg';
 
 function Audio2_5() {
